10c336a381a4f7714c47b7efbcff62a46878909d
Harsh Shandilya <me@msfjarvis.dev>
2020-07-19 03:32:29 +0530

    Replace hand-rolled logger with pretty_env_logger
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
 mod parse;
 
 use clap::{crate_name, crate_version, App, Arg, ArgGroup};
-use log::{LevelFilter, Metadata, Record};
-
-/// Simple logger that simply outputs everything using println!()
-/// It prints all levels in debug builds, and nothing on release builds.
-struct StdOutLogger;
-
-impl log::Log for StdOutLogger {
-    fn enabled(&self, _: &Metadata) -> bool {
-        cfg!(debug_assertions)
-    }
-
-    fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!("{} - {}", record.level(), record.args());
-        }
-    }
-
-    fn flush(&self) {}
-}
-
-static LOGGER: StdOutLogger = StdOutLogger;
 
 fn main() {
-    let _ = log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info));
+    pretty_env_logger::init();
     let matches = App::new(crate_name!())
         .version(crate_version!())
         .author("Harsh Shandilya <me@msfjarvis.dev>")
