bbe7545ba77eaeb5c53809440e0fccd6fde8c4e5
Harsh Shandilya <me@msfjarvis.dev>
2020-12-27 20:29:35 +0530

    cli: add support for channel argument
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod package;
 mod parse;
 mod stability;
 
+use channel::Channel;
 use clap::{crate_authors, crate_description, crate_name, crate_version, AppSettings, Clap};
 
 #[derive(Clap)]
@@ -15,13 +16,18 @@ use clap::{crate_authors, crate_description, crate_name, crate_version, AppSetti
     setting = AppSettings::DeriveDisplayOrder,
 )]
 pub(crate) struct Cli {
-    #[clap(default_value = "")]
+    /// search term to filter packages with
+    #[clap(required = true)]
     pub(crate) search_term: String,
+    /// the release channel to find packages from
+    #[clap(short='c', long="channel", possible_values=&["dev", "alpha", "beta", "rc", "stable"], default_value="alpha")]
+    pub(crate) channel: Channel,
 }
 
 fn main() {
     pretty_env_logger::init();
     let cli = Cli::parse();
+    let _ = &cli.channel;
     let packages = crate::parse::parse(&cli.search_term);
     if packages.is_empty() {
         println!("No results found!");
