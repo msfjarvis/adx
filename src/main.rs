e8bb9d83ebb79ed454d4a46161ce8fa710da7caf
Aditya Wasan <adityawasan55@gmail.com>
2021-01-17 14:28:24 +0530

    Add search and project subcommands
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,28 @@ use color_eyre::Result;
     setting = AppSettings::DeriveDisplayOrder,
 )]
 pub(crate) struct Cli {
+    #[clap(subcommand)]
+    subcmd: SubCommand
+}
+
+
+#[derive(Clap)]
+#[clap(
+    name = crate_name!(),
+    version = crate_version!(),
+    author = crate_authors!(),
+    about = crate_description!(),
+    setting = AppSettings::ColoredHelp,
+    setting = AppSettings::DeriveDisplayOrder,
+)]
+pub(crate) enum SubCommand {
+    Search(Search),
+    Project(Project)
+}
+
+/// Search individual packages
+#[derive(Clap)]
+pub(crate) struct Search {
     /// search term to filter packages with
     #[clap(required = true)]
     pub(crate) search_term: String,
@@ -24,11 +46,20 @@ pub(crate) struct Cli {
     pub(crate) channel: Channel,
 }
 
-fn main() -> Result<()> {
-    pretty_env_logger::init();
-    color_eyre::install()?;
-    let cli = Cli::parse();
-    let packages = crate::parse::parse(&cli.search_term, cli.channel)?;
+/// Search packages present in a gradle project
+#[derive(Clap)]
+pub(crate) struct Project {
+    /// Project path
+    #[clap(required=true)]
+    pub(crate) project_path: String,
+    /// the release channel to find packages from
+    #[clap(short='c', long="channel", possible_values=&["dev", "alpha", "beta", "rc", "stable"], default_value="alpha")]
+    pub(crate) channel: Channel,
+}
+
+fn search_package(search_term: String, channel: Channel) -> Result<()> {
+    let packages = crate::parse::parse(&search_term, channel)?;
+
     if packages.is_empty() {
         println!("No results found!");
     } else {
@@ -36,5 +67,32 @@ fn main() -> Result<()> {
             println!("{}", package);
         }
     };
+
+    Ok(())
+}
+
+fn search_project(project_path: String, channel: Channel) -> Result<()> {
+    /*
+     * 1. Handle OS differences and reach project_path
+     * 2. Extract dependencies using ./gradlew dependencies
+     * 3. Parse dependencies and create a list
+     * 4. Delete temp dependencies file
+     * 5. Pass the list to search_package() along with the channel specified
+     */
+    println!("path: {}", project_path);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+    color_eyre::install()?;
+    let cli = Cli::parse();
+
+    match cli.subcmd {
+        SubCommand::Search(search) => search_package(search.search_term, search.channel),
+        SubCommand::Project(project) => search_project(project.project_path, project.channel)
+    };
+
     Ok(())
 }
