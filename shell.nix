82fe9ecacae7cdfaa1a7627d7a5ebbf77d9c558f
Harsh Shandilya <me@msfjarvis.dev>
2022-03-28 13:11:25 +0530

    nix: update shell config

--- shell.nix
+++ shell.nix
@@ -1,10 +1,13 @@
 with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   buildInputs = [
-    (rust-bin.selectLatestNightlyWith (toolchain:
-      toolchain.default.override {
-        extensions = [ "rust-src" "rustfmt-preview" ];
-      }))
+    (rust-bin.stable.latest.default.override {
+      extensions = [ "rust-src" "rustfmt-preview" ];
+      targets =
+        pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
+        ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+        [ "x86_64-unknown-linux-musl" ];
+    })
   ] ++ lib.optionals stdenv.isDarwin [
     pkgs.darwin.apple_sdk.frameworks.Security
   ];
