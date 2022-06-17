fc8b2814f6fb20ff9b693bc37856fa770807c347
Harsh Shandilya <me@msfjarvis.dev>
2022-06-17 12:58:14 +0530

    feat(nix): add clippy extension

--- shell.nix
+++ shell.nix
@@ -2,12 +2,15 @@ with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   buildInputs = [
     (rust-bin.stable.latest.default.override {
-      extensions =
-        [ "rust-src" "rustc-dev" "rustfmt-preview" "llvm-tools-preview" ];
-      targets =
-        pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
-        ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-        [ "x86_64-unknown-linux-gnu" ];
+      extensions = [
+        "clippy"
+        "llvm-tools-preview"
+        "rust-src"
+        "rustc-dev"
+        "rustfmt-preview"
+      ];
+      targets = lib.optionals stdenv.isDarwin [ "aarch64-apple-darwin" ]
+        ++ lib.optionals stdenv.isLinux [ "x86_64-unknown-linux-gnu" ];
     })
   ] ++ lib.optionals stdenv.isDarwin [
     pkgs.darwin.apple_sdk.frameworks.Security
