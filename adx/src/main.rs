d8f6c1878d5925b87114ff0b549d30ad5fb9d81d
Harsh Shandilya <me@msfjarvis.dev>
2025-08-07 20:01:51 +0530

    fix: remove `measure-alloc` feature

--- adx/src/main.rs
+++ adx/src/main.rs
@@ -1,28 +1,16 @@
 mod channel;
 mod package;
 mod parse;
-#[cfg(feature = "measure-alloc")]
-mod stats_alloc;
 mod version;
-
-#[cfg(feature = "measure-alloc")]
-use std::alloc::System;
 
 use channel::Channel;
 use clap::Parser;
 use color_eyre::Result;
-#[cfg(feature = "measure-alloc")]
-use stats_alloc::{INSTRUMENTED_SYSTEM, Region, StatsAlloc};
-
-#[cfg(feature = "measure-alloc")]
-#[global_allocator]
-static GLOBAL: &StatsAlloc<System> = &INSTRUMENTED_SYSTEM;
 
 #[derive(Parser)]
 #[command(author, version, about)]
 pub(crate) struct Cli {
     /// search term to filter packages with
-    #[cfg(not(feature = "measure-alloc"))]
     #[arg(required = true)]
     pub(crate) search_term: String,
     /// the release channel to find packages from
@@ -33,12 +21,7 @@ pub(crate) struct Cli {
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
-    #[cfg(feature = "measure-alloc")]
-    let reg = Region::new(GLOBAL);
     let cli = Cli::parse();
-    #[cfg(feature = "measure-alloc")]
-    let packages = parse::parse("", cli.channel).await?;
-    #[cfg(not(feature = "measure-alloc"))]
     let packages = parse::parse(&cli.search_term, cli.channel).await?;
     if packages.is_empty() {
         println!("No results found!");
@@ -47,8 +30,6 @@ async fn main() -> Result<()> {
             println!("{package}");
         }
     }
-    #[cfg(feature = "measure-alloc")]
-    println!("{:#?}", reg.change());
     Ok(())
 }
 
