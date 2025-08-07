68b891fd034f0154d5bfbb8207af2bf5df3b2095
Harsh Shandilya <me@msfjarvis.dev>
2025-08-07 21:04:46 +0530

    feat: add `--print-includes` flag

---
+++ adx/src/exclusions.rs
@@ -0,0 +1,63 @@
+use crate::{channel::Channel::Alpha, parse::parse};
+use clap::{ValueEnum, builder::PossibleValue};
+use std::fmt::Write;
+
+#[derive(Debug, Eq, PartialEq, PartialOrd, Clone, Copy)]
+pub(crate) enum PrintType {
+    IncludeGroup,
+    IncludeModule,
+}
+
+impl ValueEnum for PrintType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::IncludeGroup, Self::IncludeModule]
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        match self {
+            PrintType::IncludeGroup => Some(PossibleValue::new("group")),
+            PrintType::IncludeModule => Some(PossibleValue::new("module")),
+        }
+    }
+}
+
+pub(crate) async fn print_inclusions(print_type: PrintType) {
+    let packages = parse("", Alpha).await;
+    let Ok(packages) = packages else { return };
+    let mut rules = String::new();
+    match print_type {
+        PrintType::IncludeGroup => {
+            packages
+                .into_iter()
+                .map(|pkg| pkg.group_id)
+                .collect::<std::collections::HashSet<_>>()
+                .into_iter()
+                .for_each(|group_id| {
+                    let _ = writeln!(rules, "includeGroup(\"{group_id}\")");
+                });
+        }
+        PrintType::IncludeModule => {
+            for pkg in packages {
+                let _ = writeln!(
+                    rules,
+                    "includeModule(\"{}\", \"{}\")",
+                    pkg.group_id, pkg.artifact_id
+                );
+            }
+        }
+    }
+    println!(
+        "
+    dependencyResolutionManagement {{
+      repositories {{
+        exclusiveContent {{
+          forRepository {{ google() }}
+          filter {{
+            {rules}
+          }}
+        }}
+      }}
+    }}
+    ",
+    );
+}
